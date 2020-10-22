260a43f7973a543a9fc1998614fae2aa9b223405
Rodrigo Oliveira Silva <rodrigo.silvoliveira@gmail.com>
2020-10-22 08:48:40 -0300

    v - 0.03

---
+++ openMP/hello.c
@@ -0,0 +1,25 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void Hello(void);
+
+int main(int argc, char *argv[])
+{
+    int thread_count = strtol(argv[1], NULL, 10);
+
+    printf("%d\n",thread_count);
+
+#pragma omp parrallel num_threads(thread_count)
+    Hello();
+
+    return 0;
+}
+
+void Hello(void)
+{
+    int my_rank = omp_get_thread_num();
+    int thread_count = omp_get_num_threads();
+
+    printf("Hello from thread %d of %d\n", my_rank, thread_count);
+}
